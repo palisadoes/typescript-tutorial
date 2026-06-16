7756b7f32bc34dfa86e36c01e511b105670b60fc
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-06-15 21:21:49 -0400

    Added .gitignore file

--- public/app.js
+++ public/app.js
@@ -10,26 +10,26 @@ var Invoice = /** @class */ (function () {
         this.amount = amount;
     }
     Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+        return "".concat(this.client, " owes \u00A3").concat(this.amount, " for ").concat(this.details);
     };
     return Invoice;
 }());
-var invOne = new Invoice('mario', 'work on the mario website', 250);
-var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invOne = new Invoice("mario", "work on the mario website", 250);
+var invTwo = new Invoice("luigi", "work on the luigi website", 300);
 var invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 invoices.forEach(function (inv) {
     console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
 });
-var form = document.querySelector('.new-item-form');
+var form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
