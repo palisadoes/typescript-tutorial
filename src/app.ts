bbbc9cbba52cb049aaf0eb7d55193c3d5102cccb
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-06-16 10:55:54 -0400

    Added .gitignore file

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,32 @@
-import { Invoice } from './classes/Invoice.js';
-import { Payment } from './classes/Payment.js';
-import { ListTemplate } from './classes/ListTemplate.js';
-import { HasFormatter } from './interfaces/HasFormatter.js';
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
 // list template instance
-const ul = document.querySelector('ul')!;
+const ul = document.querySelector("ul")!;
 const list = new ListTemplate(ul);
 
-form.addEventListener('submit', (e: Event) => {
+form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
   let doc: HasFormatter;
-  if (type.value === 'invoice') {
+  if (type.value === "invoice") {
     doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  
-  list.render(doc, type.value, 'end');
+
+  list.render(doc, type.value, "end");
 });
 
 // GENERICS
@@ -41,12 +41,12 @@ form.addEventListener('submit', (e: Event) => {
 //   return {...obj, uid};
 // }
 
-const addUID = <T extends {name: string}>(obj: T) => {
+const addUID = <T extends { name: string }>(obj: T) => {
   let uid = Math.floor(Math.random() * 100);
-  return {...obj, uid};
-}
+  return { ...obj, uid };
+};
 
-let docOne = addUID({name: 'yoshi', age: 40});
+let docOne = addUID({ name: "yoshi", age: 40 });
 //let docTwo = addUID('shaun');
 
 console.log(docOne.name);
@@ -59,15 +59,15 @@ interface Resource<T> {
 }
 
 const docThree: Resource<object> = {
-  uid: 1, 
-  resourceName: 'person', 
-  data: { name: 'shaun' }
+  uid: 1,
+  resourceName: "person",
+  data: { name: "shaun" },
 };
 
 const docFour: Resource<string[]> = {
-  uid: 1, 
-  resourceName: 'shoppingList', 
-  data: ['bread', 'milk']
+  uid: 1,
+  resourceName: "shoppingList",
+  data: ["bread", "milk"],
 };
 
 console.log(docThree, docFour);
