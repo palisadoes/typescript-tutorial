79ffe0f7c275b8750fefed37cfc2eab55ac66025
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-06-16 10:58:30 -0400

    Added .gitignore file

--- public/app.js
+++ public/app.js
@@ -1,26 +1,26 @@
-import { Invoice } from './classes/Invoice.js';
-import { Payment } from './classes/Payment.js';
-import { ListTemplate } from './classes/ListTemplate.js';
-const form = document.querySelector('.new-item-form');
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+const form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
 // list template instance
-const ul = document.querySelector('ul');
+const ul = document.querySelector("ul");
 const list = new ListTemplate(ul);
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
-    if (type.value === 'invoice') {
+    if (type.value === "invoice") {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    list.render(doc, type.value, 'end');
+    list.render(doc, type.value, "end");
 });
 // ENUMS
 var ResourceType;
@@ -29,17 +29,17 @@ var ResourceType;
     ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
     ResourceType[ResourceType["FILM"] = 2] = "FILM";
     ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
 })(ResourceType || (ResourceType = {}));
-;
 const docOne = {
     uid: 1,
     resourceType: ResourceType.BOOK,
-    data: { title: 'name of the wind' }
+    data: { title: "name of the wind" },
 };
 const docTwo = {
     uid: 10,
     resourceType: ResourceType.DIRECTOR,
-    data: { title: 'name of the wind' }
+    data: { title: "name of the wind" },
 };
 console.log(docOne);
 console.log(docTwo);
