bfb31fad9626fa5483c237d71e436d3bff2e6ce5
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-06-16 10:28:39 -0400

    Added .gitignore file

--- public/app.js
+++ public/app.js
@@ -1,23 +1,24 @@
-import { Invoice } from './classes/Invoice.js';
-import { Payment } from './classes/Payment.js';
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing', 200);
+// docOne = new Invoice("yoshi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing", 200);
 // let docs: HasFormatter[] = [];
 // docs.push(docOne);
 // docs.push(docTwo);
-const form = document.querySelector('.new-item-form');
+// console.log(docs);
+const form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
-    if (type.value === 'invoice') {
+    if (type.value === "invoice") {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
