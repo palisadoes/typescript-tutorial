fb7a38817fb26c9fc730cf89b6eae40ce86bc032
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-06-15 21:00:02 -0400

    Added .gitignore file

--- public/app.js
+++ public/app.js
@@ -7,25 +7,26 @@ var Invoice = /** @class */ (function () {
         this.amount = a;
     }
     Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+        return "".concat(this.client, " owes \u00A3").concat(this.amount, " for ").concat(this.details);
     };
     return Invoice;
 }());
-var invOne = new Invoice('mario', 'work on the mario website', 250);
-var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invOne = new Invoice("mario", "work on the mario website", 250);
+var invTwo = new Invoice("luigi", "work on the luigi website", 300);
+console.log(invOne, invTwo);
 var invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 // invoices.push({ name: 'shaun' });
 console.log(invoices);
-var form = document.querySelector('.new-item-form');
+var form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ class Invoice {
   details: string;
   amount: number;
 
-  constructor(c: string, d: string, a: number){
+  constructor(c: string, d: string, a: number) {
     this.client = c;
     this.details = d;
     this.amount = a;
@@ -15,33 +15,29 @@ class Invoice {
   }
 }
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+console.log(invOne, invTwo);
 
 let invoices: Invoice[] = [];
-invoices.push(invOne)
+invoices.push(invOne);
 invoices.push(invTwo);
 // invoices.push({ name: 'shaun' });
 
 console.log(invoices);
 
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form.addEventListener('submit', (e: Event) => {
+form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
