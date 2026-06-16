f596eb2c89725b74d31f5fc6ea6b63f09ea92806
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-06-16 10:43:59 -0400

    Added .gitignore file

--- public/classes/ListTemplate.js
+++ public/classes/ListTemplate.js
@@ -3,14 +3,14 @@ export class ListTemplate {
         this.container = container;
     }
     render(item, heading, pos) {
-        const li = document.createElement('li');
-        const h4 = document.createElement('h4');
+        const li = document.createElement("li");
+        const h4 = document.createElement("h4");
         h4.innerText = heading;
         li.append(h4);
-        const p = document.createElement('p');
+        const p = document.createElement("p");
         p.innerText = item.format();
         li.append(p);
-        if (pos === 'start') {
+        if (pos === "start") {
             this.container.prepend(li);
         }
         else {
